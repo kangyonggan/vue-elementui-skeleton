3a87ccee275d8d514e1540cdda2f175c4231b4a3
kangyonggan <java@kangyonggan.com>
2020-07-20 23:47:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,121 @@
+# vue-elementui-skeleton
+基于Vue+ElementUI的骨架屏。使用指令`v-skeleton`的方式生成骨架屏，可自动识别常用的ElementUI组件并生成对应的骨架屏。
+也提供了N多参数以便进行个性化配置。
+
+- el-table：自识别：宽度、列数、行高。可配置：行数、圆角、背景色
+- el-menu：TODO
+- el-breadcrumb：TODO
+- ...
+
+## 效果
+![效果](https://wx3.sinaimg.cn/large/0081fa71gy1ggxubw846fg30rs08ct8w.jpg)
+
+## 安装
+npm i vue-elementui-skeleton
+
+## 引入
+```js
+import Vue from 'vue';
+import VueElementUISkeleton from 'vue-elementui-skeleton';
+
+Vue.use(VueElementUISkeleton);
+
+// 可以设置选项的全局默认值和指令名称
+/*
+Vue.use(VueElementUISkeleton, {
+    directiveName: 'my-skeleton',
+    rows: 10,
+    radius: 3,
+    bg: 'red'
+});
+*/
+```
+
+## 基础用法
+以表格为例，展示基础用法。
+
+```html
+<template>
+  <el-table
+    v-skeleton="loading"
+    :data="tableData"
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180"
+    />
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180"
+    />
+    <el-table-column
+      prop="address"
+      label="地址"
+    />
+  </el-table>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                loading: false,
+                tableData: []
+            };
+        },
+        mounted() {
+            // 模拟请求耗时2s
+            let that = this;
+            that.loading = true;
+            setTimeout(function () {
+                that.loading = false;
+                that.tableData = [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }];
+            }, 2000);
+        }
+    };
+</script>
+```
+
+## 配置行数
+el-table默认骨架屏的行数为5行，下例修改为10行。
+
+```html
+<template>
+  <el-table
+    v-skeleton="{loading: loading, rows: 10}"
+    :data="tableData"
+    style="width: 100%"
+  >
+    <!-- columns -->
+  </el-table>
+</template>
+```
+
+## 选项
+| 选项 | 说明 | 类型 | 可选值 | 默认值 |
+| ----- | ----- | ----- | ----- | ----- |
+| loading | true时渲染骨架屏 | Boolean | - | false |
+| rows | 行数，只对el-table生效 | Number | - | 5 |
+| radius | 圆角像素 | Number | - | 5 |
+| bg | 骨架背景色 | String | - | #eaebed |
+
+
---
+++ src/index.js
@@ -0,0 +1,90 @@
+export default {
+    install: function (Vue, opts = {}) {
+        let {directiveName = 'skeleton', rows = 5, radius = 5, bg = '#eaebed'} = opts
+        Vue.directive(directiveName, {
+            update: function (el, binding, vnode) {
+                let oldValue = binding.oldValue;
+                let value = binding.value;
+                if (typeof value !== 'object') {
+                    value = {loading: value};
+                    oldValue = {loading: oldValue};
+                }
+
+                // loading从false变成true的时候画骨架
+                if (!oldValue.loading && value.loading) {
+                    // el-table（自识别：宽度、列数、行高。可配置：行数、圆角、背景色）
+                    if ('el-table' === vnode.componentOptions.tag) {
+                        // 隐藏空数据提示
+                        let totalWidth = el.clientWidth;
+                        let emptyText = el.querySelector('.el-table__empty-block');
+                        if (emptyText) {
+                            emptyText.style.display = 'none';
+                        }
+
+                        // 计算每一列的宽度
+                        let colsWidth = [];
+                        let usedWidth = 0;
+                        let freeCount = 0;
+                        let cols = vnode.componentOptions.children;
+                        for (let i = 0; i < cols.length; i++) {
+                            colsWidth.push(cols[i].componentOptions.propsData.width * 1);
+                            if (cols[i].componentOptions.propsData.width) {
+                                usedWidth += cols[i].componentOptions.propsData.width * 1;
+                            } else {
+                                freeCount++;
+                            }
+                        }
+
+                        // 没指定宽度的列宽 = (总宽 - 已指定的列宽总和) / 未指定列宽的个数
+                        let autoWidth = (totalWidth - usedWidth) / freeCount;
+                        for (let i = 0; i < colsWidth.length; i++) {
+                            if (!colsWidth[i]) {
+                                colsWidth[i] = autoWidth;
+                            }
+                        }
+
+                        // 在tbody中画骨架
+                        let tbody = el.querySelector('.el-table__body tbody');
+                        // 行数（缺省为5）
+                        rows = value.rows || rows;
+                        // 骨架屏背景色（缺省为#eaebed）
+                        bg = value.bg || bg;
+                        // 圆角（缺省为5）
+                        radius = value.radius || radius;
+                        for (let i = 0; i < rows; i++) {
+                            let tr = document.createElement('tr');
+                            tr.className = 'skeleton-tr el-table__row';
+                            for (let j = 0; j < colsWidth.length; j++) {
+                                let td = document.createElement('td');
+                                let cell = document.createElement('div');
+                                cell.className = 'cell';
+                                let div = document.createElement('div');
+                                div.style = 'background: ' + bg + ';border-radius: ' + radius + 'px;text-indent:-999px;width:' + (Math.random() * 50 + 30) + '%;';
+                                div.appendChild(document.createTextNode('.'));
+                                cell.appendChild(div);
+                                td.appendChild(cell);
+                                tr.appendChild(td);
+                            }
+                            tbody.appendChild(tr);
+                        }
+                    }
+                }
+                // loading从true到false的时候删除骨架
+                if (oldValue.loading && !value.loading) {
+                    // el-table
+                    if ('el-table' === vnode.componentOptions.tag) {
+                        let allSkeletons = el.querySelectorAll('.skeleton-tr');
+                        let tbody = el.querySelector('.el-table__body tbody');
+                        for (let i = 0; i < allSkeletons.length; i++) {
+                            tbody.removeChild(allSkeletons[i]);
+                        }
+                        let emptyText = el.querySelector('.el-table__empty-block');
+                        if (emptyText) {
+                            emptyText.style.display = 'block';
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
